fd8b795a8bed62774752a8bfd52ec80e6bb46618
ellenlee <leechinghui.tw@gmail.com>
2016-10-14 03:25:19 +0800

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :requests do
+  	resources :outfits
+  end
+  resources :items, only: :show
+
+  namespace :admin do
+  	resources :outfits
+  	resources :items
+  	resources :users
+  	resources :tags
+  end
+
   root to: "guide#index"
 
 end
