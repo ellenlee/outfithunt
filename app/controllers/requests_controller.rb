addefda7ae374374fc45bdca45eb1fa1d2d3ba2a
ellenlee <leechinghui.tw@gmail.com>
2016-10-14 16:40:54 +0800

    revise request errors

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,23 @@
 class RequestsController < ApplicationController
-	def index
-		
+	def new
+		@request = current_user.requests.build
+		@colors = Color.all
+		@materials = Material.all
+		@categories = Category.all
+	end
+
+	def create
+		@request = current_user.requests.build(request_params)
+		if @request.save
+			redirect_to :back, notice: "新增成功！"
+		else
+			render "new"
+		end
+	end
+
+	private 
+
+	def request_params
+		params.require(:request).permit(:user_id, :color_id, :material_id, :category_id)
 	end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
+	validates_presence_of :color_id, :material_id, :category_id
+
 	belongs_to :color
 	belongs_to :material
 	belongs_to :category
