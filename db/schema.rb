95e213073866cef4f59cf30c9052662170a4aa48
ellenlee <leechinghui.tw@gmail.com>
2016-10-24 02:42:39 +0800

    items-show: can select size and quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020163708) do
+ActiveRecord::Schema.define(version: 20161023173126) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -129,6 +129,13 @@ ActiveRecord::Schema.define(version: 20161020163708) do
     t.index ["material_id"], name: "index_requests_on_material_id"
     t.index ["style_id"], name: "index_requests_on_style_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "sizes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_sizes_on_name"
   end
 
   create_table "styles", force: :cascade do |t|
