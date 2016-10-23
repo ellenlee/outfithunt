573edcf367eb85db1a12c979a7642471a18b37e0
ellenlee <leechinghui.tw@gmail.com>
2016-10-24 04:08:25 +0800

    my shelf view structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
 
   def profile
     @requests = current_user.requests
-    @outfits = current_user.collected_outfits
+
+    @outfits_odd = Array.new
+    @outfits_even = Array.new
+
+    outfits = current_user.collected_outfits
+    outfits.each_with_index do |outfit, index|
+      if index % 2 == 1
+        @outfits_odd << outfit
+      else
+        @outfits_even << outfit
+      end
+    end
   end
 end
