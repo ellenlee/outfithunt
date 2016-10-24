8a32e22b883792136ee0e1a2c2dd3fc2cc692902
ellenlee <leechinghui.tw@gmail.com>
2016-10-24 18:31:30 +0800

    cancel authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate
+  # before_action :authenticate
   before_action :authenticate_user!
 
   protected
-  def authenticate
-    authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == "outfithunt" && password == "outfithunt2016"
-  	end
-  end
+  # def authenticate
+    # authenticate_or_request_with_http_basic do |user_name, password|
+      # user_name == "outfithunt" && password == "outfithunt2016"
+  	# end
+  # end
 end
